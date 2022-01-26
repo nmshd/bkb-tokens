7996abade74ed434cd31289631fef62fac2b1b1a
tnotheis <timo.notheisen@js-soft.com>
2022-01-26 10:21:03 +0100

    chore: use conventions

--- Tokens.Infrastructure/Persistence/Database/EntityConfigurations/TokenEntityTypeConfiguration.cs
+++ Tokens.Infrastructure/Persistence/Database/EntityConfigurations/TokenEntityTypeConfiguration.cs
@@ -11,10 +11,6 @@ public class TokenEntityTypeConfiguration : IEntityTypeConfiguration<Token>
     {
         builder.HasIndex(r => r.CreatedBy);
 
-        builder.Property(x => x.Id).HasColumnType($"char({TokenId.MAX_LENGTH})");
-        builder.Property(x => x.CreatedBy).HasColumnType($"char({IdentityAddress.MAX_LENGTH})");
-        builder.Property(x => x.CreatedByDevice).HasColumnType($"char({DeviceId.MAX_LENGTH})");
-
         builder.Ignore(a => a.Content);
     }
 }
